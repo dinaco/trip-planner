fd18db56f0681df7e12473e2f85fb56edd7d4d99
Dino Marchiori <dinaco1@gmail.com>
2022-05-12 17:06:44 +0100

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("trip-planner JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/trip-planner";
+
+module.exports = MONGO_URI;
