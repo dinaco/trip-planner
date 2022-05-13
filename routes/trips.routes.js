a4dac839a88ddb624e818cb1e5c4d859326c0842
Dino Marchiori <dinaco1@gmail.com>
2022-05-13 17:09:00 +0100

    changes on trips routes and views

--- routes/trips.routes.js
+++ routes/trips.routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 const moment = require("moment");
 
 // Require the User model in order to interact with the database
-const User = require("../models/User.model");
+//const User = require("../models/User.model");
 
 // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 const isLoggedIn = require("../middleware/isLoggedIn");
