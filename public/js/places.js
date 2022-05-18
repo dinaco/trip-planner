ea444d3abd159fef5e0e1c1d7fe5e5ddba90c588
Chloé Faurie <chloe.faurie@gmail.com>
2022-05-18 14:40:09 +0100

    add country bounds to accomodation search

--- public/js/places.js
+++ public/js/places.js
@@ -8,6 +8,7 @@ function initSearch() {
   }; */
 
   // ------------ for city ---------------//
+  let cityCountry, lat, lng, defaultBounds, autocompleteAccomodation;
   const cityInput = document.getElementById("cityName");
   const cityOptions = {
     types: ["(cities)"],
@@ -27,8 +28,11 @@ function initSearch() {
     console.log(place);
     document.getElementById("cityLocationLat").value =
       place.geometry.location.lat();
+    lat = place.geometry.location.lat();
+    console.log(lat, typeof lat);
     document.getElementById("cityLocationLng").value =
       place.geometry.location.lng();
+    lng = place.geometry.location.lng();
     document.getElementById("photoUrl").value = place.photos[0].getUrl({
       maxWidth: 350,
       maxHeight: 350,
@@ -42,28 +46,49 @@ function initSearch() {
     infowindowContent.children["place-address"].textContent =
       place.formatted_address; */
     /*     infowindow.open(map, marker); */
+    let i = place.address_components.length - 1;
+    cityCountry = place.address_components[i].short_name;
+    cityCountry = cityCountry; //.toLowerCase();
+    console.log(cityCountry + " is of type: " + typeof cityCountry);
+
+    defaultBounds = {
+      north: Number(lat + 0.2),
+      south: Number(lat - 0.2),
+      east: Number(lng + 0.2),
+      west: Number(lng - 0.2),
+    };
+
+    const accomodationInput = document.getElementById("accomodation");
+    console.log(defaultBounds);
+    const accomodationOptions = {
+      bounds: defaultBounds,
+      componentRestrictions: { country: cityCountry },
+      types: ["establishment"],
+    };
+
+    autocompleteAccomodation = new google.maps.places.Autocomplete(
+      accomodationInput,
+      accomodationOptions
+    );
+    loadAccomodationAutocomplete();
   });
 
+  // console.log(latCity, lngCity);
   // ------------ for accomodation ---------------//
-  const accomodationInput = document.getElementById("accomodation");
-  const accomodationOptions = {
-    types: ["establishment"],
-  };
-  const autocompleteAccomodation = new google.maps.places.Autocomplete(
-    accomodationInput,
-    accomodationOptions
-  );
+  function loadAccomodationAutocomplete() {
+    autocompleteAccomodation.addListener("place_changed", () => {
+      console.log(`This is printed?: ` + cityCountry, lat, lng, defaultBounds);
 
-  autocompleteAccomodation.addListener("place_changed", () => {
-    autocompleteAccomodation.getPlace();
-    const place = autocompleteAccomodation.getPlace();
-    console.log(place);
-    console.log(place.name);
-    document.getElementById("accomodationLocationLat").value =
-      place.geometry.location.lat();
-    document.getElementById("accomodationLocationLng").value =
-      place.geometry.location.lng();
+      autocompleteAccomodation.getPlace();
+      const place = autocompleteAccomodation.getPlace();
+      console.log(place);
+      console.log(place.name);
+      document.getElementById("accomodationLocationLat").value =
+        place.geometry.location.lat();
+      document.getElementById("accomodationLocationLng").value =
+        place.geometry.location.lng();
 
-    document.getElementById("accomodationName").value = place.name;
-  });
+      document.getElementById("accomodationName").value = place.name;
+    });
+  }
 }
