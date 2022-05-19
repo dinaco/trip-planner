b315d14305357bb3fe26eec7a2b54b29ee3b2c07
Dino Marchiori <dinaco1@gmail.com>
2022-05-19 15:49:07 +0100

    active nav-link

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,14 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    console.log("trip-planner JS imported successfully!");
+    let nav = document.querySelector(".navbar");
+    let navLink = document.querySelectorAll(".nav-link");
+    const currentPath = location.pathname;
+    for (let i = 0; i < navLink.length; i++) {
+      if (currentPath === navLink[i].getAttribute("href")) {
+        navLink[i].parentNode.classList.add("active");
+      }
+    }
   },
   false
 );
